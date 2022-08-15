3f6d11211d0a7dc8ff14f20a37a33aa2edf6837d
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-15 16:02:21 +0530

    add the auth listener hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const NotFound = lazy(() => import("./pages/not-found"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login}></Route>
           <Route path={ROUTES.SIGN_UP} component={SignUp}></Route>
+          <Route path={ROUTES.DASHBOARD} component={Dashboard}></Route>
+          <Route component={NotFound}></Route>
         </Switch>
       </Suspense>
     </Router>
---
+++ src/components/timeline.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function timeline() {
+  return <div>timeline</div>;
+}
+
+export default timeline;
