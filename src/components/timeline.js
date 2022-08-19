97056859532c2283463ac8fd1c4e93104209b13a
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-19 20:00:34 +0530

    Skeleton working on the homepage YEAHHHH!

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,10 +1,23 @@
 import React from "react";
 import usePhotos from "../hooks/use-photos";
+import Skeleton from "react-loading-skeleton";
 
 function Timeline() {
   const { photos } = usePhotos();
   console.log("PHOTOS === ", photos);
-  return <div>timeline</div>;
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-4" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">
+          Follow People to see some photos!
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default Timeline;
