b4a98d47b2fd9c7485646474bb83b28154ddf310
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-20 10:10:52 +0530

    Routing is finally working - had to remove the strict mode -idk why

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import { firebase, FieldValue } from "./lib/firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-      <App />
-    </FirebaseContext.Provider>
-  </React.StrictMode>
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>
 );
